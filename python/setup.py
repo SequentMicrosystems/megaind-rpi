ac53aa1f01935b2ea0ea363fa7a09be2ee6beb2d
3lv <cristian.vlad.vladutu@gmail.com>
2025-02-26 23:49:16 +0200

    comply with PEP 625

--- python/setup.py
+++ python/setup.py
@@ -14,9 +14,9 @@
 
 from setuptools import setup, find_packages
 setup(
-    name='SMmegaind',
+    name='smmegaind',
     packages=find_packages(),
-    version='1.0.3',
+    version='1.0.4',
     license='MIT',
     description='Library to control PLACEHOLDER Automation Card',
     #long_description=long_description,
